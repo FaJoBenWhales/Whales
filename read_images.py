53cd3ddac7fe867d397ff6be9ff0e8cac7e4a1c9
Ben <BenWilhelm@gmx.net>
2018-01-18 17:14:26 +0100

    added some code with imageio and scykit-image libraries. Imageio is for loading and storing images, scykit-image aka skimage is for processing images.

--- read_images.py
+++ read_images.py
@@ -1,20 +1,66 @@
 # Deep learning lab course final project.
 # Kaggle whale classification.
 
-# Read jpeg images into numpy arrays.
+# routines to handle the images
 
-# We cannot use this like this, the uncompressed files get too large!
 
 import os
 import pickle
 import numpy as np
-from scipy.misc import imread
+from scipy.misc import imread as depricated_imread  # works, but derpicated, see documentation
+import imageio  # see https://imageio.github.io/ and http://imageio.readthedocs.io/en/latest/userapi.html
+import skimage.color  # see http://scikit-image.org/docs/stable/api/api.html
+
+#
+#
+#   FUNCTIONS
+#
+#
+
+
+def load_jpg(path):
+    """
+    Loads a jpg to a numpy array.
+    The shape of the numpy array is
+    (x, y, 3) for RGB images and
+    (x, y)    for grayvalue images.
+    :param path: the file-path of the jpg
+    :return: the numpy array representing the jpg
+    """
+    image = imageio.imread(path)
+    # print(image.meta)
+    # print(image.shape)
+    return image
+
+
+def test_imageio():
+    """
+    simple test of imageio. loads and writes files.
+    """
+    image = load_jpg("./data/small_train/0a5c0f48.jpg")
+    imageio.imwrite('ch0.jpg', image[:, :, 0])
+    imageio.imwrite('ch1.jpg', image[:, :, 1])
+    imageio.imwrite('ch2.jpg', image[:, :, 2])
+    imageio.imwrite('chA.jpg', image)
+
+
+def test_skimage():
+    """
+    simple test of skimage. loads and writes files.
+    """
+    image = load_jpg("./data/small_train/0a5c0f48.jpg")
+    # convert RGB to grayscale
+    greyimage = skimage.color.rgb2gray(image)
+    imageio.imwrite("greyimage.jpg", greyimage)
 
 
 def pickle_images(folder="data/train", outfile="data/train.pkl"):
-    """Read jpeg images from folder and save in python format to
-outfile. The format used is a pickled numpy array containing the
-individual images as numpy arrays. """
+    """
+    Don't use this, the uncompressed files get too large!
+    Read jpeg images from folder and save in python format to
+    outfile. The format used is a pickled numpy array containing the
+    individual images as numpy arrays.
+    """
     images = []
     files = os.listdir(folder)
     num_files = len(files)
@@ -22,18 +68,24 @@ individual images as numpy arrays. """
         file = os.path.join(folder, fn)
         print("Decompressing file {} of {}: {}".format(i, num_files, file))
         if os.path.isfile(file):
-            images.append(imread(file))
+            images.append(depricated_imread(file))
     images = np.array(images)
     print("Saving in numpy/pickle format.")
     with open(outfile, "wb") as output:
         pickle.dump(images, output)
     print("Done reformatting images.")
-    
-    
-def load_images(path="data/train.pkl"):
-    with open("train.pkl", "rb") as infile: 
+
+
+def pickle_load_images(path="data/train.pkl"):
+    with open(path, "rb") as infile:
         return pickle.load(infile)
 
-# pickle_images("data/train")
-pickle_images("data/small_train")
-print(load_images())
+
+#
+#
+#   GLOBAL CODE
+#
+#
+
+# test_imageio()
+# test_skimage()
