4720e64d9748ee53af6b4c7ddc20d2b99727f818
FaJoBenWhales <35464429+FaJoBenWhales@users.noreply.github.com>
2018-01-26 18:41:56 +0100

    utilties updates TestKeras running, but no training so far

--- utilities.py
+++ utilities.py
@@ -42,8 +42,9 @@ def write_csv(csv_list, file_name = "data/small_train.csv"):
 # generate sorted list clustered by individuals: 
 # (name, number of images, array of indeces into train_list)
 def get_whales(train_list):
-    
+
     train_arr = np.asarray(train_list)
+    
     whale_names = np.unique(train_arr.T[1], axis=0)
 
     whales = []
@@ -128,7 +129,8 @@ def create_small_case(sel_whales = [1,2,3],          # whales to be considered
                       train_dir = "data/train",
                       train_csv = "data/train.csv",
                       small_dir = "data/small_train", 
-                      small_csv = None):         # optionally write CSV File as train.csv
+                      small_csv = None,         # optionally write CSV File as train.csv
+                      sub_dirs = True):         # create subdirectory for each class
 
     if not os.path.isdir(train_dir):
         print("{} no valid directory".format(train_dir))
@@ -141,24 +143,40 @@ def create_small_case(sel_whales = [1,2,3],          # whales to be considered
         print("directory {} did not exist so far".format(small_dir))
         
     os.mkdir(small_dir)
+    if not sub_dirs:
+        target_dir = os.path.join(small_dir, "whales")
+        os.mkdir(target_dir)
+        
     small_list=[]
     train_list = read_csv(file_name = train_csv)  # get list with (filenames, whalenames)
     whales, counts = get_whales(train_list)   # get list of whales ordered by frequency
     for i in sel_whales:                          
-        # whale_idx = whales[i][2]                 # get list of indices of this whale
         print("copy {} images for whale # {} in ordered list, called {}"
               .format(whales[i][1], i, whales[i][0]))
-        whale_dir = os.path.join(small_dir, whales[i][0])
-        os.mkdir(whale_dir)
+        if sub_dirs:
+            target_dir = os.path.join(small_dir, whales[i][0])
+            os.mkdir(target_dir)
         for idx in whales[i][2]:   # array of indices of this whale pointing into train_csv list
             fn = train_list[idx][0]     # get filename out of train_csv list
             shutil.copy(os.path.join(train_dir, fn), 
-                        os.path.join(whale_dir, fn))
+                        os.path.join(target_dir, fn))
             
             small_list.append((fn,whales[i][0]))
     if small_csv != None:
         print("write csv file: {}".format(small_csv))            
         write_csv(small_list, small_csv)
+    
+    print(len(small_list), " images of ", len(sel_whales), " whales copied")
+    return len(small_list)  # return number of images
+
+
+# create dictionary mapping class_no as used by Keras in predictions --> whalenames
+def make_label_dict(directory="data/model_train"):
+    label_dict = dict()
+    for i, label in enumerate(sorted(os.listdir(directory))):
+        label_dict[i] = label
+    return label_dict
+
 
 
 '''
@@ -192,3 +210,64 @@ def mean_average_precision(model_predictions, true_labels, max_pred):
     return np.mean([average_precision(mp, tl, max_pred) 
                     for mp, tl in zip(model_predictions, true_labels)])
 
+'''
+As a benchmark create case of a dumb model without any predictive power and test, how this model performs
+measured in MAP@5 metric, by building 
+1) a dummy set of true labels and 
+2) a dummy model prediction (max pred ranked predictions per sample) 
+and applying mean_average_precision() on these
+
+
+compute:
+1) a "dumb" model, that maps the images to the individuals uniform randomly, 
+2) a model, that maps the images to the individuals along a probability distribution 
+   that reflects the distribution of frequencies of observed whales as given in "train.csv"
+   --> whale #1, will occure 34 times more likely than whale #2.500 with one image
+3) like 2), but without the category "new whale" (~800 occurences) 
+'''
+from scipy.stats import rv_discrete
+
+def Dummy_MAP(probs = 'uniform', 
+              distributed_as = "data/train.csv",    # csv file with mimiked distribution
+              max_pred = 5,
+              image_no = 100):
+    
+    dist_list = read_csv(file_name = distributed_as)   # for testing whole train data set
+
+    whales, counts = get_whales(dist_list)
+    if probs == 'weighted_without_first':   # leaving out first whale "new whale" with 810 individuals
+        whales = whales[1:]
+        counts = counts[1:]
+    total_whales = np.sum(counts)
+    sorted_whales = np.arange(0,len(whales))     
+
+    # create dummy list of true labels following probability distribution
+    dummy_true_labels = []
+    # probability distribution = normalised distribution of frequencies in dist_list
+    px=[whales[i][1]/total_whales for i in range(len(whales))]
+    for i in range(image_no):
+        # sample labels according probability distribution defined above
+        dummy_true_labels.append(rv_discrete(values=(sorted_whales,px)).rvs(size=1)[0])
+    
+    # to each image in dummy predictions map a ranked list of max_pred whales
+    # as random number between 1 and # of individuals in scenario (indeces in whale list)
+    # following the distribution as given in "distributed_as"
+    dummy_preds = []
+    if probs == 'uniform':
+        for i in range(image_no):
+            dummy_preds.append(np.random.randint(0,len(counts),max_pred))
+
+    # mimik prob. distribution from distribution in train.csv
+    elif probs == 'weighted' or probs == 'weighted_without_first':  
+        # probability distribution = normalised distribution of frequencies
+        px=[whales[i][1]/total_whales for i in range(len(whales))]
+        # create ranked list of max_pred = 5 labels per image in train_list
+        for i in range(image_no):
+            # sample labels according to probability distribution defined above
+            ranks=rv_discrete(values=(sorted_whales,px)).rvs(size=max_pred)  
+            dummy_preds.append(ranks)
+            
+    else:
+        raise AssertionError
+
+    return mean_average_precision(dummy_preds, dummy_true_labels, max_pred)
