598f6aae6053a492024e2680e551420b1494f7bc
Ben <BenWilhelm@gmx.net>
2018-02-02 11:09:59 +0100

    cleaned up keras_model and keras_tools

--- keras_model.py
+++ keras_model.py
@@ -11,7 +11,6 @@ from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras import optimizers
-from keras import backend as K
 
 import utilities as ut
 import keras_tools as tools
@@ -24,12 +23,12 @@ def create_pretrained_model(config_dict, num_classes=10):
     num_layers = config_dict['num_layers']
     num_units = []
     for i in range(num_layers):
-        num_units[i] = config_dict['num_units_' + str(i)]
+        num_units.append(config_dict['num_units_' + str(i)])
     learning_rate = config_dict['learning_rate']
-    optimizer = config_dict['activation']
+    optimizer = config_dict['optimizer']
     dropout = []
     for i in range(num_layers):
-        dropout[i] = config_dict['dropout_' + str(i)]
+        dropout.append(config_dict['dropout_' + str(i)])
     
     # load pre-trained model
     if base_model == 'InceptionV3':
@@ -62,7 +61,7 @@ def create_pretrained_model(config_dict, num_classes=10):
     elif optimizer == 'Adam':
         opt = optimizers.Adam(lr=learning_rate)
     elif optimizer == 'RMSProp':
-        opt = optimizers.RMSProp(lr=learning_rate)
+        opt = optimizers.RMSprop(lr=learning_rate)
     else:
         raise NotImplementedError("Unknown optimizer: {}".format(optimizer))
         
@@ -72,24 +71,37 @@ def create_pretrained_model(config_dict, num_classes=10):
     return model
 
 
-def train_cross_validation(model, epochs=20, cross_validation_iterations=5,
-                           num_classes=10, save_path=None):
-    histories = []
-    for i in range(cross_validation_iterations):
-        path_components = save_path.split(os.extsep)
-        path = ".".join(path_components[:-1] + [i] + path_components[-1])
-        hist = train_and_save(model, epochs, num_classes, save_path)
-        histories.append(hist)
-    return histories
+def unfreeze_cnn_layers(model):
+    # we chose to train the top 2 inception blocks, i.e. we will freeze
+    # the first 249 layers and unfreeze the rest:
+    for layer in model.layers[:249]:
+       layer.trainable = False
+    for layer in model.layers[249:]:
+       layer.trainable = True
+
+    # we need to recompile the model for these modifications to take effect
+    # we use SGD with a low learning rate
+    from keras.optimizers import SGD
+    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+    # model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+    
+    print("\n ****** unfrozen 2 top CNN layers ******")
+    return model
 
 
-def train(model, config_dict, epochs=20, num_classes=10, save_path=None,
-          train_dir="data/model_train", valid_dir="data/model_valid"):
+def train(model, num_classes, config_dict, epochs=20, cnn_epochs = 0, save_path=None,
+          train_dir="data/model_train", valid_dir="data/model_valid", train_valid_split=0.7):
+    
     batch_size = config_dict['batch_size']
     
     # create new environment with new random train / valid split
-    num_train_imgs, num_valid_imgs = tools.prepare_environment(num_classes)
-
+    num_train_imgs, num_valid_imgs = ut.create_small_case(
+        sel_whales=np.arange(1, num_classes+1),
+        train_dir=train_dir,
+        valid_dir=valid_dir,
+        train_valid=train_valid_split,
+        sub_dirs=True)
+            
     train_gen = image.ImageDataGenerator(
         # featurewise_center=True,
         # featurewise_std_normalization=True,
@@ -127,33 +139,57 @@ def train(model, config_dict, epochs=20, num_classes=10, save_path=None,
         validation_steps=num_valid_imgs//batch_size,
         epochs=epochs)
 
+    history = hist.history    
+    
+    if cnn_epochs > 0:
+        model = unfreeze_cnn_layers(model)
+        hist_cnn = model.fit_generator(
+            train_flow, 
+            steps_per_epoch = num_train_imgs//batch_size,
+            verbose = 2, 
+            validation_data = valid_flow,   # to be used later
+            validation_steps = num_valid_imgs//batch_size,
+            epochs=cnn_epochs)
+
+        for key in history.keys():
+            if type(history[key]) == list:
+                history[key].extend(hist_cnn.history[key])
+
     if save_path is not None:
         model.save(save_path)
-        
-    return hist.history
+    
     # TODO: we need to return (loss, runtime, learning_curve)
+    return history
 
 
 def main():
-    print("Run complete script: Printing data info, train, print test results.")
-    tools.print_data_info()
-    model = create_pretrained_model()
-    histories = train(model, epochs=2)
+    print("Run short training with InceptionV3 and save results.")
+    num_classes = 10
+    config_dict = {'base_model': 'InceptionV3', 
+                   'activation': 'relu', 
+                   'num_layers': 2,
+                   'num_units_0': 1024,
+                   'num_units_1': 1024,
+                   'dropout_0': 0.5,
+                   'dropout_1': 0.5,
+                   'learning_rate': 0.001,
+                   'optimizer': 'RMSProp',
+                   'batch_size': 16}
+    model = create_pretrained_model(config_dict, num_classes=num_classes)
+    histories = train(model, num_classes, config_dict, epochs=2, cnn_epochs=2)
+    print("HISTORIES:")
     print(histories)
     run_name = tools.get_run_name()
     tools.save_learning_curves(histories, run_name)
+    csv_path = os.path.join("plots/", run_name, "data.csv")
     ut.write_csv_dict(histories,
                       keys=['loss', 'acc', 'val_loss', 'val_acc'],
-                      filename=run_name + '.csv')
-    tools.print_model_test_info(model)
+                      filename=csv_path)
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         main()
-        exit()
-    if "--prepare-only" in sys.argv:
-        tools.prepare_environment()
         exit()
     if "--class-graph" in sys.argv:
         tools.draw_num_classes_graphs()
