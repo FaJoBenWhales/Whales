0804466a3b99648c7b02e3f84c873186ddf7a4a6
Fabian Gloeckle <gloeckf@tfpool43.informatik.uni-freiburg.de>
2018-02-04 18:31:33 +0100

    Fix bugs in random_crop.py.

--- random_crop.py
+++ random_crop.py
@@ -5,6 +5,7 @@ import numpy as np
 import os.path
 from scipy.misc import imresize
 from keras.preprocessing.image import array_to_img
+from keras import backend as K
 
 def random_crop_generator(image_generator, target_size=(299, 299),
                           max_crop_x=0.5, max_crop_y=0.5,
@@ -26,18 +27,21 @@ def random_crop_generator(image_generator, target_size=(299, 299),
             "Preserving aspect ratio is currently not available.")
     
     for batch_images, batch_labels in image_generator:
-        new_images = np.zeros_like(batch_images)
         length = batch_images.shape[0]
-        
+        new_images = np.zeros(tuple([length] +
+                                    list(target_size) +
+                                    list(batch_images.shape)[3:]),
+                              dtype=K.floatx())
+                
         for i in range(length):
             image = batch_images[i]
             
             # calculate crop indices
-            size_x, size_y = image.shape[:1]
-            lose_x = np.random.randint(size_x * max_crop_x)
-            lose_y = np.random.randint(size_y * max_crop_y)
-            start_x = np.random.randint(lose_x)
-            start_y = np.random.randint(lose_y)
+            size_x, size_y = image.shape[:2]
+            lose_x = np.random.randint(size_x * max_crop_x + 1)
+            lose_y = np.random.randint(size_y * max_crop_y + 1)
+            start_x = np.random.randint(lose_x + 1)
+            start_y = np.random.randint(lose_y + 1)
             end_x = size_x - lose_x + start_x
             end_y = size_y - lose_y + start_y
             
@@ -47,7 +51,7 @@ def random_crop_generator(image_generator, target_size=(299, 299),
             if save_to_dir:
                 img = array_to_img(image)
                 fname = '{hash}.png'.format(hash=np.random.randint(1e7))
-                img.save(os.path.join(self.save_to_dir, fname))
+                img.save(os.path.join(save_to_dir, fname))
                 
             image = imresize(image, size=target_size,
                              interp=interpolation_method)
